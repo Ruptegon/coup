8d498ecbfcab191634dd4666396a96da23e7f779
Kacper Szkodzinski <kacper.szkodzinski@outlook.com>
2023-05-21 18:13:20 +0200

    Create GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using Coup.GameLogic;
+using UnityEngine;
+
+namespace Coup
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField]
+        private int playerCount;
+
+        private GameEngine engine;
+
+        void Start()
+        {
+            engine = new GameEngine();
+            engine.SetupGame(playerCount);
+        }
+    }
+}
